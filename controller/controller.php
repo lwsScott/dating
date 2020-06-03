5db0a3bb07be6f57753ce61709870331ebec2bae
Lewis Scott <lscott19@mail.greenriver.edu>
2020-06-02 21:40:42 -0400

    edited to allow premium to provide photo

--- controller/controller.php
+++ controller/controller.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Lewis Scott
+ * 4/17/20
+ * filename https://lscott.greenriverdev.com/328/dating/controller/controller.php
+ * Controller page for dating website
+ * provides routes to various views and runs fat free
+ */
+require '/home2/lscottgr/config.php';
 
 /**
  * Class MemberController
@@ -151,8 +159,12 @@ class MemberController
                 $this->_f3->set('selectedState', $_POST['state']);
             }
 
-            $this->_f3->set('selectedGender', $_POST['seeking']);
-            $this->_f3->set('selectedBio', $_POST['bio']);
+            if (isset($_POST['seeking'])) {
+                $this->_f3->set('selectedGender', $_POST['seeking']);
+            }
+            if (!empty($_POST['bio'])) {
+                $this->_f3->set('selectedBio', $_POST['bio']);
+            }
 
 
             if ($valid)
@@ -170,10 +182,17 @@ class MemberController
                 $_SESSION['member']->setBio($_POST['bio']);
 
 
-                //Redirect to the interests route
-                $this->_f3->reroute("interests");
-                //session_destroy();
+                //Redirect to the interests route if premium member
+                if (get_class($_SESSION['member']) == "PremiumMember")
+                {
+                    $this->_f3->reroute("interests");
+                }
+                else {
+                    //redirect to summary page
+                    $this->_f3->reroute('summary');
+                }
             }
+
         }
 
         // if method is get display a page called profile.html
@@ -194,39 +213,55 @@ class MemberController
         $this->_f3->set('outdoorInts', $outdoorInts);
         if($_SERVER['REQUEST_METHOD'] == 'POST')
         {
-            //var_dump($_POST);
+            if (isset($_POST['indoorInts']))
+            {
+                $selectedIndoor = $_POST['indoorInts'];
+            }
+            else
+            {
+                $selectedIndoor = "";
+            }
+            if (isset($_POST['outdoorInts']))
+            {
+                $selectedOutoor = $_POST['outdoorInts'];
+            }
+            else
+            {
+                $selectedOutoor = "";
+            }
+
             // validate the data
             $valid = true;
-            if (!$this->_validator->validIndoor($_POST['indoorInts']))
+            if (!$this->_validator->validIndoor($selectedIndoor))
             {
                 $valid = false;
                 $this->_f3->set('errors["indoorInts"]', "Invalid choice(s)");
                 // make the valid ones sticky
-                $f3->set('selectedIndoor', $_POST['indoorInts']);
+                $this->_f3->set('selectedIndoor', $selectedIndoor);
             } else {
-                $this->_f3->set('selectedIndoor', $_POST['indoorInts']);
+                $this->_f3->set('selectedIndoor', $selectedIndoor);
             }
 
-            if (!$this->_validator->validOutdoor($_POST['outdoorInts']))
+            if (!$this->_validator->validOutdoor($selectedOutoor))
             {
                 $valid = false;
                 $this->_f3->set('errors["outdoorInts"]', "Invalid choice(s)");
                 // make the valid ones sticky
-                $this->_f3->set('selectedOutdoor', $_POST['outdoorInts']);
+                $this->_f3->set('selectedOutdoor', $selectedOutoor);
             } else {
-                $this->_f3->set('selectedOutdoor', $_POST['outdoorInts']);
+                $this->_f3->set('selectedOutdoor', $selectedOutoor);
             }
 
             if ($valid) {
                 // store the data in the session array
                 //$_SESSION['outdoorInts'] = $_POST['outdoorInts'];
                 //$_SESSION['indoorInts'] = $_POST['indoorInts'];
-                $_SESSION['member']->setIndoorInts($_POST['indoorInts']);
-                $_SESSION['member']->setOutdoorInts($_POST['outdoorInts']);
+                $_SESSION['member']->setIndoorInts($selectedIndoor);
+                $_SESSION['member']->setOutdoorInts($selectedOutoor);
 
-                //redirect to summary page
-                $this->_f3->reroute('summary');
-                session_destroy();
+                //redirect to image page
+                $this->_f3->reroute('image');
+                //session_destroy();
             }
         }
 
@@ -236,15 +271,144 @@ class MemberController
     }
 
     /**
+     * Display the profile image route
+     * establish database connection and store image
+     * to a folder and its filename to a database if chosen
+     * route to summary
+     */
+    public function image()
+    {
+        try {
+            //Create a new PDO connection
+            $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "Connected!";
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+        //echo "here in the controller image";
+        $dirName = '../uploader/uploads/';
+        $_SESSION['dirName'] = $dirName;
+        //$target_file = $dirName . basename($_FILES["fileToUpload"]["name"]);
+        //$uploadOk = 1;
+
+        //echo realpath("test.txt");
+
+        //echo $dirName;
+        //var_dump($_FILES['fileToUpload']);
+        //If file has been submitted
+        if (isset($_FILES['fileToUpload'])) {
+
+            $file = $_FILES['fileToUpload'];
+            $_SESSION['member']->setImageId($file['name']);
+            //echo $file['name'];
+
+            //echo $file['name'].'<br>';
+            //echo $file['type'].'<br>';
+            //echo $file['tmp_name'].'<br>';
+            //echo $file['error'].'<br>';
+            //echo $file['size'].'<br>';
+
+            // 264.JPG
+            // image/jpeg
+            // /tmp/phpCSZfKz
+            // 0
+            // 2632637
+
+            //Define valid file types
+            $validTypes = array('image/jpeg', 'image/jpg', 'image/png');
+
+            //Check file size - 3 MB maximum
+            if ($_SERVER['CONTENT_LENGTH'] > 3000000) {
+                echo "<p class='error'>File is too large. Maximum file size is 3 MB.</p>";
+            } //Check file type
+            else if (in_array($file['type'], $validTypes)) {
+
+                if ($file['error'] > 0)
+                    echo "<p class='error'>Return Code: {$file['error']}</p>";
+
+                //Check for duplicate file
+                if (file_exists($dirName . $file['name'])) {
+                    echo "<p class='error'>Error uploading: ";
+                    echo $file['name'] . " already exists.</p>";
+                } else {
+                    //Move file to upload directory
+                    move_uploaded_file($file['tmp_name'], $dirName . $file['name']);
+                    echo "<p class='success'>Uploaded {$file['name']} successfully!</p> ";
+
+                    // store the file name in the database
+                    $sql = "INSERT INTO uploads(filename) VALUES ('{$file['name']}')";
+                    $dbh->exec($sql);
+
+                    // establish the $id number of the last insert
+                    $id = $dbh->lastInsertId();
+                    $_SESSION['id'] = $id;
+
+                    //echo $id;
+
+                    //echo $dir;
+
+                    //Get names of files
+                    //$sql = "SELECT * FROM uploads
+                    //        WHERE file_id = $id";
+                    //echo $sql;
+                    //$result = $dbh->query($sql);
+                    //var_dump($result);
+                    //$img = $result['filename'];
+                    //$_SESSION['img'] = $img;
+                    //echo $file['name'];
+                    //$_SESSION['member']->setImageId($file['name']);
+
+                    //var_dump($result);
+                    //redirect to image page
+                    $this->_f3->reroute('summary');
+
+                }
+            }
+            //Invalid file type
+            else {
+                echo "<p class='error'>Invalid file type. Allowed types: gif, jpg, png</p>";
+            }
+        }
+        /*
+         * Show each image
+         */
+        /*
+
+
+        //Display images
+        if (sizeof($result) >= 1) {
+            foreach ($result as $row) {
+                $img = $row['filename'];
+                echo "<img class='img-thumbnail' src='$dirName$img' alt=''>";
+            }
+        }
+        */
+
+        $view = new Template();
+        //var_dump($view);
+        echo $view->render('views/imageUpload.php');
+    }
+
+    /**
      *
      */
     public function summary()
     {
+
+
+        //Open file directory
+        $dir = opendir($_SESSION['dirName']);
+        //$id = $_SESSION['id'];
+        //$img = $_SESSION['member']->getImageId();
+        //$dirName = $_SESSION['dirName'];
+        //echo "<img class='img-thumbnail' src='$dirName$img' alt=''>";
+
         // display the summary page
         $view = new Template();
         echo $view->render
         ('views/summary.php');
-        $_SESSION = array();
         session_destroy();
     }
 
--- views/summary.php
+++ views/summary.php
@@ -23,7 +23,8 @@ error_reporting(E_ALL);
 
 
 <div class="container card rounded">
-
+    <!-- Display member Summary  -->
+    <!-- If Premium member Display Interests  -->
     <div class="row">
         <div class="col-6">
             <p class="card mb-0 ml-3 mt-3 p-2">Name: {{ @SESSION.member->getFname() }} {{@SESSION.member->getLname() }}</p>
@@ -33,18 +34,30 @@ error_reporting(E_ALL);
             <p class="card mb-0 ml-3 p-2">E-Mail : {{ @SESSION.member->getEmail() }}</p>
             <p class="card mb-0 ml-3 p-2">State : {{ @SESSION.member->getState() }}</p>
             <p class="card mb-0 ml-3 p-2">Seeking : {{ @SESSION.member->getSeeking() }}</p>
-            <p class="card mb-0 ml-3 p-2">Interests :
-                <check if="{{ !empty(@SESSION.member->getIndoorInts()) }}">
-                    {{ implode(@SESSION.member->getIndoorInts(), ', ') }}
-                </check><check if="{{ !empty(@SESSION.member->getIndoorInts()) }}">
-                    <check if="{{ !empty(@SESSION.member->getOutdoorInts()) }}">,</check>
-                    {{ implode(@SESSION.member->getOutdoorInts(), ', ') }}
-                </check>
-            </p>
+            <check if="{{ get_class($_SESSION['member']) == 'PremiumMember' }}">
+                <p class="card mb-0 ml-3 p-2">Interests :
+                    <check if="{{ !empty(@SESSION.member->getIndoorInts()) }}">
+                        {{ implode(@SESSION.member->getIndoorInts(), ', ') }}
+                    </check><check if="{{ !empty(@SESSION.member->getIndoorInts()) }}">
+                        <check if="{{ !empty(@SESSION.member->getOutdoorInts()) }}">,</check>
+                        {{ implode(@SESSION.member->getOutdoorInts(), ', ') }}
+                    </check>
+                </p>
+            </check>
         </div>
 
+        <!-- If Premium member has profile photo display that  -->
+        <!--   -->
         <div class="col-6">
-            <img class="img-fluid rounded" src="images/homePageImage.png" alt="image">
+            <check if="{{ get_class($_SESSION['member']) == 'PremiumMember' && !empty(@SESSION.member->getImageId()) }}">
+                <true>
+                    <img class="img-fluid rounded m-2 p-2" src="{{ @SESSION.dirName}}{{@SESSION.member->getImageId()}}" alt="image">
+                </true>
+                <false>
+                    <img class="img-fluid rounded m-2 p-2" src="images/homePageImage.png" alt="image">
+                </false>
+            </check>
+
             <p>Biography</p>
             <p> {{ @SESSION.member->getBio() }}</p>
 
@@ -53,8 +66,8 @@ error_reporting(E_ALL);
 
     <div class="text-center ml-3 mr-3">
         <!--<form action="#" method="get">-->
-            <button class="btn btn-primary text-center m-3" type="submit">Contact Me!</button>
-       <!-- </form>-->
+        <button class="btn btn-primary text-center m-3" type="submit">Contact Me!</button>
+    <!-- </form>-->
     </div>
 
 </div>
